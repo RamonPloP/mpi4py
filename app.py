2a24956ab1c0b55c867a44f684eb8eaa68c8ec54
RamonPloP <aspireandlive@gmail.com>
2024-04-18 23:15:03 -0600

    Se agrega entorno virtual y termina de binary search

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request
+from mpi4py import MPI
+import numpy as np
 
 app = Flask(__name__)
+
+def binary_search(arr, target):
+
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
 
 @app.route('/')
 
@@ -34,10 +49,42 @@ def binary():
 @app.route('/binaryres', methods=["POST"])
 
 def binaryres():
-    array = request.form['datos']
+    # Arreglo de ejemplo (debe estar ordenado)
+    arr = np.array(list(map(int, request.form['datos'].split(','))))
+    arr.sort()
+    sorted(arr)
+    # Elemento a buscar
+    target = int(request.form['target'])
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    # Dividir el arreglo en partes iguales
+    chunk_size = len(arr) // size
+    local_arr = np.zeros(chunk_size, dtype=int)
+
+    # Distribuir partes del arreglo a cada proceso
+    comm.Scatter(arr, local_arr, root=0)
+
+    # Realizar búsqueda binaria en cada parte del arreglo
+    result = binary_search(local_arr, target)
+
+    # Reunir los resultados de todos los procesos en el proceso raíz
+    all_results = comm.gather(result, root=0)
+
+    # Mostrar el resultado en el proceso raíz
+    if rank == 0:
+        for i, res in enumerate(all_results):
+            if res != -1:
+                msg = f"El elemento {target} fue encontrado en el proceso {i} en el índice {res}."
+            else:
+                msg = f"El elemento {target} no fue encontrado en el proceso {i}."
+
     data = {
     "title": "Binary Search",
-    "datos": array
+    "msg": msg
+
 }
     return render_template('binaryres.html', data=data)
 
